0e0c3420bdf97e50baf321512250f9941bcb4af2
sabotenpillow <saboten.pillow@gmail.com>
2018-02-14 07:13:44 +0000

    TCP

--- orcinusorca.cc
+++ orcinusorca.cc
@@ -4,10 +4,11 @@
 #include <libnetfilter_queue/libnetfilter_queue.h>
 #include <iomanip>
 #include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
+#include <netinet/ip_icmp.h>
 #include <arpa/inet.h>
-#include <cstdint>
 #include <linux/if_ether.h>
-#include <stdio.h>
 
 #define QUEUE_ID 10
 #define HexFormat(wd, fill) std::hex<<std::setw(wd)<<std::setfill(fill)
@@ -20,20 +21,45 @@ using std::endl;
 static void printhex(const char *buf, int len) {
   int i;
   for (i = 0; i < len; i++) {
-    cout << HexFormat(2, '0') << (buf[i] & 0xff) << ' ';
+    cout <<HexFormat(2,'0')<< (buf[i] & 0xff) <<' ';
     if ((i & 0x0f) == 0x0f)
       cout<<endl;
   }
+  cout<<std::dec;
 }
 
 int cb(struct nfq_q_handle *qh, struct nfgenmsg *msg, struct nfq_data *nfdata, void *data) {
   struct nfqnl_msg_packet_hdr *header;
   char *payload;
   int len;
+  unsigned int datasize;
+  std::string buf;
 
   header = nfq_get_msg_packet_hdr(nfdata);
   len = nfq_get_payload(nfdata, (unsigned char **)&payload);
-  printhex(payload, len);
+  struct iphdr *iph = (struct iphdr *)payload;
+  cout << inet_ntoa(*(struct in_addr *)&iph->saddr) << " -> "
+       << inet_ntoa(*(struct in_addr *)&iph->daddr);
+  switch ( iph->protocol ) {
+  case IPPROTO_TCP: {
+      struct tcphdr *tcph = (struct tcphdr *)(payload + sizeof(struct iphdr));
+      datasize = iph->tot_len - (iph->ihl<<10) - (tcph->doff<<10);
+      buf = std::string(payload
+              + (iph->ihl<<2) + (tcph->doff<<2)).substr(0, datasize);
+      cout << " TCP ";
+      cout << ntohs(tcph->source) << "->"
+           << ntohs(tcph->dest) <<endl;
+      cout << buf;
+    } break;
+  case IPPROTO_UDP: {
+      cout << " UDP ";
+    } break;
+  case IPPROTO_ICMP: {
+      cout << " ICMP ";
+    } break;
+  default: break;
+  }
+  //printhex(payload, len);
 
   return nfq_set_verdict(qh, ntohl(header->packet_id), NF_ACCEPT, 0, NULL);
 }
@@ -70,7 +96,7 @@ int main(void) {
   fd = nfq_fd(h);
   cout << "file descriptor is " << fd << endl;
   cout << "start nfq handle" << endl;
-  cout << endl;
+  cout<<endl;
   while ((rv = recv(fd, buf, sizeof(buf), 0)) >= 0) {
     cout << endl << " == getpacket ==" << endl;
     nfq_handle_packet(h, buf, rv);
