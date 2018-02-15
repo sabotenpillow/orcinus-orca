34cc2529af5f37c66cc55f5551af4749e11597ea
sabotenpillow <saboten.pillow@gmail.com>
2018-02-15 14:54:47 +0000

    display on TUI with ncurses

--- Makefile
+++ Makefile
@@ -1,9 +1,10 @@
 CC := clang++
 CFLAGS := -Wall -std=c++1y -stdlib=libc++
+LIB := -lnetfilter_queue -lncurses
 #CC := g++
 #CFLAGS := -Wall -std=c++1y
 
 build: orcinusorca.cc
-	$(CC) $(CFLAGS) -o orcinusorca orcinusorca.cc -lnetfilter_queue
+	$(CC) $(CFLAGS) -o orcinusorca orcinusorca.cc $(LIB)
 clean:
 	rm orcinusorca
--- orcinusorca.cc
+++ orcinusorca.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include <netinet/in.h>
 #include <linux/netfilter.h>
 #include <libnetfilter_queue/libnetfilter_queue.h>
@@ -9,6 +10,7 @@
 #include <netinet/ip_icmp.h>
 #include <arpa/inet.h>
 #include <linux/if_ether.h>
+#include <ncurses.h>
 
 #define QUEUE_ID 10
 #define HexFormat(wd, fill) std::hex<<std::setw(wd)<<std::setfill(fill)
@@ -17,6 +19,9 @@ using std::cin;
 using std::cout;
 using std::cerr;
 using std::endl;
+
+static int x, y;
+static int pktlist_i = 0;
 
 static void printhex(const char *buf, int len) {
   int i;
@@ -34,31 +39,35 @@ int cb(struct nfq_q_handle *qh, struct nfgenmsg *msg, struct nfq_data *nfdata, v
   int len;
   unsigned int datasize;
   std::string buf;
+  std::stringstream disp_line;
 
   header = nfq_get_msg_packet_hdr(nfdata);
   len = nfq_get_payload(nfdata, (unsigned char **)&payload);
   struct iphdr *iph = (struct iphdr *)payload;
-  cout << inet_ntoa(*(struct in_addr *)&iph->saddr) << " -> "
-       << inet_ntoa(*(struct in_addr *)&iph->daddr);
+  disp_line << inet_ntoa(*(struct in_addr *)&iph->saddr) << " -> "
+            << inet_ntoa(*(struct in_addr *)&iph->daddr);
   switch ( iph->protocol ) {
   case IPPROTO_TCP: {
       struct tcphdr *tcph = (struct tcphdr *)(payload + sizeof(struct iphdr));
       datasize = iph->tot_len - (iph->ihl<<10) - (tcph->doff<<10);
       buf = std::string(payload
               + (iph->ihl<<2) + (tcph->doff<<2)).substr(0, datasize);
-      cout << " TCP ";
-      cout << ntohs(tcph->source) << "->"
-           << ntohs(tcph->dest) <<endl;
-      cout << buf;
+      disp_line << " TCP ";
+      disp_line << ntohs(tcph->source) << "->"
+                << ntohs(tcph->dest) <<endl;
+      disp_line << buf;
     } break;
   case IPPROTO_UDP: {
-      cout << " UDP ";
+      disp_line << " UDP ";
     } break;
   case IPPROTO_ICMP: {
-      cout << " ICMP ";
+      disp_line << " ICMP ";
     } break;
   default: break;
   }
+  mvprintw(pktlist_i, 0, disp_line.str().c_str());
+  refresh();
+  pktlist_i+=1;
   //printhex(payload, len);
 
   return nfq_set_verdict(qh, ntohl(header->packet_id), NF_ACCEPT, 0, NULL);
@@ -94,14 +103,18 @@ int main(void) {
     exit(1);}
 
   fd = nfq_fd(h);
-  cout << "file descriptor is " << fd << endl;
-  cout << "start nfq handle" << endl;
-  cout<<endl;
+  //cout << "file descriptor is " << fd << endl;
+  //cout << "start nfq handle" << endl;
+  //cout<<endl;
+
+  initscr();
+  getmaxyx(stdscr, y, x);
   while ((rv = recv(fd, buf, sizeof(buf), 0)) >= 0) {
-    cout << endl << " == getpacket ==" << endl;
+    //cout << endl << " == getpacket ==" << endl;
     nfq_handle_packet(h, buf, rv);
   }
 
+  endwin();
   if (nfq_unbind_pf(h, AF_INET) < 0) {
     cerr << "error during nfq_unbind_pf()" << endl;
     exit(1);}
